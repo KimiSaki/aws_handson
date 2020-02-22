aeef721593d263178a39aa1a258e0e0ab86480f0
Saki Kimiduka <s.kimizuka@hotmail.co.jp>
2020-02-22 23:58:19 +0900

    handon3'

--- translate-function.py
+++ translate-function.py
@@ -1,19 +1,30 @@
+import os
 import json
 import boto3
 
 
 def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    file_text = obj['Body'].read()
+    file_dict = json.loads(file_text)
+    input_text = file_dict['results']['transcripts'][0]['transcript']
+    print(input_text)
     
     translate = boto3.client('translate')
-    input_text = event['queryStringParameters']['input_text']
-    
     response = translate.translate_text(
         Text=input_text,
-        SourceLanguageCode='ja',
-        TargetLanguageCode='en'
+        SourceLanguageCode='en',
+        TargetLanguageCode='ja'
         )
     
     output_text = response.get('TranslatedText')
+    print(output_text)
+    output_file_name = os.path.splitext(os.path.basename(key))[0] + '_ja.txt'
+    s3.put_object(Body=output_text, Bucket=bucket, Key=output_file_name)
 
     return {
         'statusCode': 200,
